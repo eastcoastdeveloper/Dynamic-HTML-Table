1a982f85db5aaf8bf82a63032098b2c6e6595308
eastcoastdeveloper <58962475+eastcoastdeveloper@users.noreply.github.com>
2023-02-09 09:59:08 -0800

    1 file modified

--- index.js
+++ index.js
@@ -48,12 +48,12 @@ function populateTable(arr) {
 function renderData(arr) {
   for (var i = 0; i < arr.length; i++) {
     markup +=
-      `<div class="tble-rows">` +
-      `<div class="tble-cells">${arr[i].id}</div>` +
-      `<div class="tble-cells">${arr[i].brand}</div>` +
-      `<div class="tble-cells">${arr[i].category}</div>` +
-      `<div class="tble-cells">${arr[i].title}</div>` +
-      `<div class="tble-cells">${arr[i].price}</div>` +
+    '<div class="tble-rows">' +
+    '<div class="tble-cells">' + arr[i].id + '</div>' +
+    '<div class="tble-cells">' + arr[i].brand + '</div>' +
+    '<div class="tble-cells">' + arr[i].category + '</div>' +
+    '<div class="tble-cells">' + arr[i].title + '</div>' +
+    '<div class="tble-cells">' + arr[i].price + '</div>' +
       `</div>`;
   }
 }
